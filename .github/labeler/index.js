9b92e4e0de99cd35545005f5d7e2a762517641e0
Breezewish <me@breeswish.org>
2021-07-08 18:35:00 +0800

    Add label logic

--- .github/labeler/index.js
+++ .github/labeler/index.js
@@ -1,10 +1,34 @@
 const github = require("@actions/github");
 const core = require("@actions/core");
 
+async function doAddLabels(labels) {
+  if (!github.context.payload.pull_request) {
+    return;
+  }
+  const token = core.getInput("githubToken");
+  const octokit = github.getOctokit(token);
+  await octokit.rest.issues.addLabels({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.repo.issue_number,
+    labels: labels,
+  });
+}
+
 try {
-  // const token = core.getInput('githubToken');
-  const payload = JSON.stringify(github.context.payload, null, 2);
-  console.log(`The event payload: ${payload}`);
+  if (!github.context.payload.pull_request) {
+    console.log("Not a Pull Request event, skipped");
+    return;
+  }
+
+  const title = github.context.payload.pull_request.title;
+  if (title.startsWith("Update TiDB Dashboard to")) {
+    doAddLabels(["component/visualization", "require-LGT1"]);
+    return;
+  }
+
+  console.log("Not matching any label rules, skip");
 } catch (error) {
-  core.setFailed(error.message);
+  console.error(error.message);
+  // Do not set errors
 }
