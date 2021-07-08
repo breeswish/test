23051390e70b0c180d7e7d93d666b1945ba291a9
Breezewish <me@breeswish.org>
2021-07-08 18:39:17 +0800

    More debug info

--- .github/labeler/index.js
+++ .github/labeler/index.js
@@ -5,8 +5,16 @@ async function doAddLabels(labels) {
   if (!github.context.payload.pull_request) {
     return;
   }
+  console.log("Adding labels", labels);
   const token = core.getInput("githubToken");
   const octokit = github.getOctokit(token);
+  console.log({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: github.context.repo.issue_number,
+    labels: labels,
+  });
+  console.log(github.context.repo);
   await octokit.rest.issues.addLabels({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
@@ -15,20 +23,24 @@ async function doAddLabels(labels) {
   });
 }
 
-try {
-  if (!github.context.payload.pull_request) {
-    console.log("Not a Pull Request event, skipped");
-    return;
-  }
+async function run() {
+  try {
+    if (!github.context.payload.pull_request) {
+      console.log("Not a Pull Request event, skipped");
+      return;
+    }
 
-  const title = github.context.payload.pull_request.title;
-  if (title.startsWith("Update TiDB Dashboard to")) {
-    doAddLabels(["component/visualization", "require-LGT1"]);
-    return;
-  }
+    const title = github.context.payload.pull_request.title;
+    if (title.startsWith("Update TiDB Dashboard to")) {
+      await doAddLabels(["component/visualization", "require-LGT1"]);
+      return;
+    }
 
-  console.log("Not matching any label rules, skip");
-} catch (error) {
-  console.error(error.message);
-  // Do not set errors
+    console.log("Not matching any label rules, skip");
+  } catch (error) {
+    console.error(error.message);
+    // Do not set errors
+  }
 }
+
+run();
